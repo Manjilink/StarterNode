2c1d47d34929fa68610bb7f80852a1faf5f133d6
Mathuresh Das <mathuresh108@gmail.com>
2025-09-03 13:01:16 -0500

    Fix ChakraProvider theme error

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-import theme from './theme'
+// Remove theme import for now
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
