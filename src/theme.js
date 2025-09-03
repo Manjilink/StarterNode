1f4aac97db95ee9f4c4e34b00ae05a160a8bb242
Mathuresh Das <mathuresh108@gmail.com>
2025-09-03 12:04:09 -0500

    Initial commit

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      500: '#0967D2',
+      600: '#0552B5',
+    }
+  },
+})
+
+export default theme
