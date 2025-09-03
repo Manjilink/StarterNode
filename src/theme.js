0e73a30c685fbe8aacbed0412c0fdfa468f9e2aa
Mathuresh Das <mathuresh108@gmail.com>
2025-09-03 12:15:56 -0500

    Fix theme import

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,11 @@
-import { extendTheme } from '@chakra-ui/theme'
-
-const theme = extendTheme({
+// Simple theme without extendTheme
+const theme = {
   colors: {
     brand: {
       500: '#0967D2',
       600: '#0552B5',
     }
   },
-})
+}
 
 export default theme
